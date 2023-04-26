7afee469a7d92c3bb0e62958effcc85b262f74ad
Kari Sturgeon <sturgeonkari@gmail.com>
2023-04-26 16:30:32 -0700

    fixed the gitignore file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-requre 
+// require("./utils.js");
+
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const Joi = require("joi");
+
+const expireTime = 24;
+
+const {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+app.get('/', (req,res) => {
+    res.send("<h1>Hello World!</h1>");
+});
