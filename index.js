dcb958c9af71e74698ceab03b7c09fdde5a0c33a
Kari Sturgeon <sturgeonkari@gmail.com>
2023-05-08 16:45:28 -0700

    added a dynamic header

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const app = express();
 
 const Joi = require("joi");
 
+const url = require("url");
+
 const expireTime = 24 * 60 * 60 * 1000;
 
 const mongodb_host = process.env.MONGODB_HOST;
@@ -30,6 +32,15 @@ app.use(express.urlencoded({ extended: false }));
 const userCollection = database.db(mongodb_database).collection('users');
 
 app.set('view engine', 'ejs');
+
+const navLinks = [
+    { name: "Home", link: "/" },
+    { name: "Log In", link: "/login" },
+    { name: "Members", link: "/members" },
+    { name: "404", link: "/404" },
+    { name: "Log Out", link: "/logout" }
+
+]
 
 
 var mongoStore = MongoStore.create({
@@ -54,7 +65,7 @@ function isValidSession(req) {
     return false;
 }
 
-function sessionValidation(req,res,next) {
+function sessionValidation(req, res, next) {
     if (isValidSession(req)) {
         next();
     }
@@ -73,7 +84,7 @@ function isAdmin(req) {
 function adminAuthorization(req, res, next) {
     if (!isAdmin(req)) {
         res.status(403);
-        res.render("errorMessage", {error: "Not Authorized"});
+        res.render("errorMessage", { error: "Not Authorized", navLinks: navLinks, currentURL: url.parse(req.url).pathname });
         return;
     }
     else {
@@ -83,17 +94,17 @@ function adminAuthorization(req, res, next) {
 
 app.get('/', (req, res) => {
     if (!isValidSession(req)) {
-        res.render("index");
+        res.render("index", { navLinks: navLinks, currentURL: url.parse(req.url).pathname });
 
     }
     else {
         var name = req.session.name;
-        res.render("index-loggedin", {name: name});
+        res.render("index-loggedin", { navLinks: navLinks, currentURL: url.parse(req.url).pathname, name: name });
     }
 });
 
 app.get('/signup', (req, res) => {
-res.render("signup")
+    res.render("signup", { navLinks: navLinks, currentURL: url.parse(req.url).pathname })
 })
 
 
@@ -108,7 +119,7 @@ app.post('/signupSubmit', async (req, res) => {
         <a href='/signup'>Try Again</a>
         `
         res.send(html);
-    } 
+    }
     else if (email == "") {
         var html = `
         Please provide an email address.
@@ -123,36 +134,37 @@ app.post('/signupSubmit', async (req, res) => {
         `
         res.send(html);
     }
-    else {const schema = Joi.object(
-        {
-            name: Joi.string().alphanum().max(15).required(),
-            password: Joi.string().max(30).required(),
-            email: Joi.string().email().max(20).required()
-        });
+    else {
+        const schema = Joi.object(
+            {
+                name: Joi.string().alphanum().max(15).required(),
+                password: Joi.string().max(30).required(),
+                email: Joi.string().email().max(20).required()
+            });
 
 
 
-    const validationResult = schema.validate({name, password, email});
-    error = validationResult.error;
-    if (validationResult.error != null) {
-        console.log("error" + validationResult.error); 
-        var html = `
+        const validationResult = schema.validate({ name, password, email });
+        error = validationResult.error;
+        if (validationResult.error != null) {
+            console.log("error" + validationResult.error);
+            var html = `
         Please provide valid input for all fields.
         <a href='/signup'>Try Again</a>
         `
-        res.send(html);
-        return; 
-    }
+            res.send(html);
+            return;
+        }
 
-    var hashedPassword = await bcrypt.hash(password, saltRounds);
+        var hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    await userCollection.insertOne({ name: name, password: hashedPassword, email: email, user_type: "user" });
-    console.log("Inserted user");
-    req.session.authenticated = true;
-    req.session.email = email;
-    req.session.name = name;
-    req.session.cookie.maxAge = expireTime;
-    res.redirect('/members');
+        await userCollection.insertOne({ name: name, password: hashedPassword, email: email, user_type: "user" });
+        console.log("Inserted user");
+        req.session.authenticated = true;
+        req.session.email = email;
+        req.session.name = name;
+        req.session.cookie.maxAge = expireTime;
+        res.redirect('/members');
     }
 
 
@@ -160,59 +172,55 @@ app.post('/signupSubmit', async (req, res) => {
 
 
 app.get('/login', (req, res) => {
-res.render("login");
+    res.render("login", { navLinks: navLinks, currentURL: url.parse(req.url).pathname });
 });
 
-app.post('/loginSubmit', async (req,res) => {
+app.post('/loginSubmit', async (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-	const schema = Joi.object(
+    const schema = Joi.object(
         {
-        email: Joi.string().max(20).required(),
-        password: Joi.string().max(20).required()
+            email: Joi.string().max(20).required(),
+            password: Joi.string().max(20).required()
         }
     );
-	const validationResult = schema.validate({email, password});
-	if (validationResult.error != null) {
-	   console.log(validationResult.error);
-	   res.redirect("/login");
-	   return;
-	}
+    const validationResult = schema.validate({ email, password });
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect("/login");
+        return;
+    }
 
-	const result = await userCollection.find({email: email}).project({email: 1, password: 1, _id: 1, name: 1, user_type: 1}).toArray();
+    const result = await userCollection.find({ email: email }).project({ email: 1, password: 1, _id: 1, name: 1, user_type: 1 }).toArray();
 
-	console.log(result);
-	if (result.length != 1) {
-		console.log("user not found");
-res.render("login-error");
-		return;
-	}
-	if (await bcrypt.compare(password, result[0].password)) {
+    console.log(result);
+    if (result.length != 1) {
+        console.log("user not found");
+        res.render("login-error", { navLinks: navLinks, currentURL: url.parse(req.url).pathname });
+        return;
+    }
+    if (await bcrypt.compare(password, result[0].password)) {
         var name = result[0].name;
-		console.log("correct password");
-		req.session.authenticated = true;
-		req.session.email = email;
+        console.log("correct password");
+        req.session.authenticated = true;
+        req.session.email = email;
         req.session.name = name;
         req.session.user_type = result[0].user_type;
-		req.session.cookie.maxAge = expireTime;
-		res.redirect('/members');
-		return;
-	}
-	else {
-		console.log("incorrect password");
+        req.session.cookie.maxAge = expireTime;
+        res.redirect('/members');
+        return;
+    }
+    else {
+        console.log("incorrect password");
         res.render("login-error");
-		return;
-	}
+        return;
+    }
 });
 
 
-app.get('/members', (req, res) => {
-    if (!isValidSession(req)) {
-        res.redirect('/');
-    }
-    res.render("members");
-    console.log(req.session.user_type);
+app.get('/members', sessionValidation, (req, res) => {
+    res.render("members", { navLinks: navLinks, currentURL: url.parse(req.url).pathname });
 })
 
 app.get('/logout', (req, res) => {
@@ -222,26 +230,24 @@ app.get('/logout', (req, res) => {
 
 app.post('/users', async (req, res) => {
     const { name, action } = req.body;
-  
     if (action === 'promote') {
-      await userCollection.updateOne({ name }, { $set: { user_type: 'admin' } });
+        await userCollection.updateOne({ name }, { $set: { user_type: 'admin' } });
     } else if (action === 'demote') {
-      await userCollection.updateOne({ name }, { $set: { user_type: 'user' } });
+        await userCollection.updateOne({ name }, { $set: { user_type: 'user' } });
     }
-  
     res.redirect('/admin'); // Redirect to home page after update
-  });
+});
 
 app.get('/admin', sessionValidation, adminAuthorization, async (req, res) => {
-    const result = await userCollection.find().project({name: 1, _id: 1, user_type: 1}).toArray();
-res.render('admin', {users: result});
+    const result = await userCollection.find().project({ name: 1, _id: 1, user_type: 1 }).toArray();
+    res.render('admin', { users: result, navLinks: navLinks, currentURL: url.parse(req.url).pathname });
 })
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("*", (req, res) => {
     res.status(404);
-    res.render("404");
+    res.render("404", { navLinks: navLinks, currentURL: url.parse(req.url).pathname });
 })
 
 app.listen(port, () => {
